8c4ef7f6d73788fd6007f900d6658cdd9e529cd9
Узоров Кирилл Александрович <kirill.uzorov@tmk-group.com>
2024-09-17 16:40:41 +0300

    Полное руководство по развёртыванию

--- build-and-push.bat
+++ build-and-push.bat
@@ -1,7 +1,7 @@
 @echo off
 
 @REM REM Логинимся в Docker Hub
-@REM docker login -u uzorovkirill -p Rekid2000
+@REM docker login -u -p 
 
 @REM REM Собираем образы
 docker-compose build
@@ -12,7 +12,7 @@ docker-compose build
 @REM REM docker push cpr-ui.cr.cloud.ru/cpr-web-ui:latest
 @REM docker push uzorovkirill/api-gateway:latest
 
-docker login cpr-web-ui.cr.cloud.ru -u b110a0a98fb8aa4f8840167b19f86177 -p e46e6f1abdec564745d7f16fd4d41c66
+docker login cpr-web-ui.cr.cloud.ru -u  -p 
 
 docker push cpr-web-ui.cr.cloud.ru/cpr-web-ui:latest
 
